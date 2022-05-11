d3716357098d398271d1103420cceaf0563be3e1
Sophronia-Delphinas <651283066@qq.com>
2022-05-11 08:05:44 +0800

    文件终止校验

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 #include <fstream>
 #include <Windows.h>
 
+class EndOfFileException : public std::exception {
+
+};
+
 bool CheckIfUtf8(std::fstream& in) {
 	std::streampos pos = in.tellg();
 
 	in.seekg(0, std::ios::beg);
 	int32_t header = 0;
 	for (int i = 0; i < 3 && !in.eof(); i++) {
-		BYTE c = in.peek();
+		char c;
+		in.get(c);
 		header <<= 8;
-		header |= c;
-		in.seekg(1, std::ios::cur);
+		header |= (BYTE)c;
 	}
 
 	bool ret = header == 0xEFBBBF;
@@ -26,20 +30,25 @@ int SizeOfUnicode(BYTE header) {
 	return size;
 }
 int32_t PeekFromUtf8(std::fstream& in) {
-	BYTE c = in.peek();
-	in.seekg(1, std::ios::cur);
+	std::istream::int_type check = in.peek();
+	if (check == EOF)
+		return 0;
+	char c;
+	in.get(c);
 	int size = SizeOfUnicode(c);
 	if (!size)
-		return c;
+		return (BYTE)c;
 	if (size == 8)
 		return 0;
-	int32_t ret = c & (0xFF >> (7 - size));
+	int prevSize = 7 - size;
+	int32_t ret = ((BYTE)c) & (0xFF >> prevSize);
 	for (int i = 0; i < size - 1; i++) {
-		c = in.peek();
-		ret <<= 8;
-		ret |= c & 0x3F;
-
-		in.seekg(1, std::ios::cur);
+		in.get(c);
+		if (c == EOF)
+			throw EndOfFileException();
+		ret <<= prevSize;
+		ret |= ((BYTE)c) & 0x3F;
+		prevSize = 6;
 	}
 	return ret;
 }
@@ -49,11 +58,10 @@ int main() {
 	std::fstream out("target.txt");
 
 	if (CheckIfUtf8(in)) {
-		int32_t c = -1;
-		while (!in.eof() && c) {
-			c = PeekFromUtf8(in);
-			if (c == '\n')
-				in.seekg(1, std::ios::cur);
+		while (!in.eof()) {
+			int32_t c = PeekFromUtf8(in);
+			if (c == 0)
+				break;
 			std::cout << c << std::endl;
 		}
 	}
