0fcb4c9a0b260abc80e5c7268cc7d1c0b5c8d428
Sophronia-Delphinas <651283066@qq.com>
2022-05-11 23:28:08 +0800

    封装

--- main.cpp
+++ main.cpp
@@ -2,113 +2,175 @@
 #include <fstream>
 #include <Windows.h>
 #include <array>
+#include <vector>
 
-using UCHAR_32 = uint32_t;
+namespace cbm::core {
+    using UNICHAR = uint32_t;
+    using USTR = UNICHAR*;
+    using CUSTR = const USTR;
+}
+namespace cbm::util {
+    using namespace cbm::core;
 
-class EndOfFileException : public std::exception {
-public:
-    EndOfFileException() = default;
-    ~EndOfFileException() override = default;
-    [[nodiscard]] const char* what() const noexcept override {
-        return "Unexpected end of file";
+	class CODE_CONVERTER {
+    protected:
+        CODE_CONVERTER() = default;
+        virtual ~CODE_CONVERTER() = default;
+    public:
+        virtual bool CheckStreamHeader(std::istream& in) {
+            throw std::exception();
+        }
+        virtual UNICHAR PeekFromStream(std::istream& in) {
+            throw std::exception();
+        }
+
+        virtual UNICHAR PeekFromString(const void* source, size_t& cursor) = 0;
+        std::vector<UNICHAR> ConvertString(const void* str) {
+            std::vector<UNICHAR> vector;
+
+            size_t cursor = 0;
+            while (true) {
+                UNICHAR uc = PeekFromString(str, cursor);
+                if (!uc)
+                    break;
+                vector.push_back(uc);
+            }
+
+            return vector;
+        }
+
+        virtual int ConvertUnicode(UNICHAR c, std::array<BYTE, 4>& u8Array) = 0;
+        virtual void WriteToStream(std::ostream& out, UNICHAR c) {
+            throw std::exception();
+        }
+        void WriteUString(std::ostream& out, CUSTR uString) {
+            for (size_t i = 0; uString[i]; i++)
+                WriteToStream(out, uString[i]);
+        }
+
+        static CODE_CONVERTER& BaseUTF8Converter();
+	};
+
+    class CONV_UTF8 : public CODE_CONVERTER {
+        static int SizeOfUnicode(BYTE first) {
+            char size = 0;
+            for (BYTE t = first; t & 0x80; t <<= 1, size++);
+            return size;
+        }
+    public:
+        CONV_UTF8() = default;
+        ~CONV_UTF8() override = default;
+
+        bool CheckStreamHeader(std::istream& in) override {
+            std::streampos pos = in.tellg();
+
+            in.seekg(0, std::ios::beg);
+            int32_t header = 0;
+            for (int i = 0; i < 3 && !in.eof(); i++) {
+                char c;
+                in.get(c);
+                header <<= 8;
+                header |= (BYTE)c;
+            }
+
+            bool ret = header == 0xEFBBBF;
+            if (!ret)
+                in.seekg(pos);
+
+            return ret;
+        }
+        UNICHAR PeekFromStream(std::istream& in) override {
+            std::istream::int_type check = in.peek();
+            if (check == EOF)
+                return 0;
+            char c;
+            in.get(c);
+            int size = SizeOfUnicode(c);
+            if (!size)
+                return (BYTE)c;
+            if (size == 8)
+                return 0;
+            int32_t ret = ((BYTE)c) & (0xFF >> (7 - size));
+            for (int i = 0; i < size - 1; i++) {
+                in.get(c);
+                if (c == EOF)
+                    return 0;
+                ret <<= 6;
+                ret |= ((BYTE)c) & 0x3F;
+            }
+            return ret;
+        }
+        UNICHAR PeekFromString(const void *source, size_t& cursor) override {
+            const BYTE* str = static_cast<const BYTE *>(source);
+            if (!str[cursor])
+                return 0;
+            BYTE c = str[cursor++];
+            int size = SizeOfUnicode(c);
+            if (!size)
+                return c;
+            if (size == 8)
+                return 0;
+            int32_t ret = c & (0xFF >> (7 - size));
+            for (int i = 0; i < size - 1; i++) {
+                c = str[cursor++];
+                if (!c)
+                    return 0;
+                ret <<= 6;
+                ret |= c & 0x3F;
+            }
+            return ret;
+        }
+
+        int ConvertUnicode(UNICHAR c, std::array<BYTE, 4>& u8Array) override {
+            if (c <= 0x7F) {
+                u8Array[0] = (BYTE)c;
+                return 1;
+            }
+            int size;
+            if (c <= 0x7FF)
+                size = 2;
+            else if (c <= 0xFFFF)
+                size = 3;
+            else
+                size = 4;
+            for (int i = size - 1; i > 0; i--) {
+                BYTE tail = c & 0x3F;
+                u8Array[i] = (BYTE)(0x80 | tail);
+                c >>= 6;
+            }
+            BYTE mask = (0xFF >> (8 - size)) << (8 - size);
+            u8Array[0] = (BYTE)(mask | (BYTE)c);
+            return size;
+        }
+        void WriteToStream(std::ostream& out, UNICHAR c) override {
+            std::array<BYTE, 4> buffer = {};
+            int size = ConvertUnicode(c, buffer);
+            for (int i = 0; i < size; i++)
+                out << buffer[i];
+        }
+    };
+
+    CODE_CONVERTER& CODE_CONVERTER::BaseUTF8Converter() {
+        static CONV_UTF8 converter;
+        return converter;
     }
-};
-class IllegalUtf8Exception : public std::exception {
-public:
-    IllegalUtf8Exception() = default;
-    ~IllegalUtf8Exception() override = default;
-    [[nodiscard]] const char* what() const noexcept override {
-        return "Illegal UTF8 character";
-    }
-};
-
-bool CheckIfUtf8(std::fstream& in) {
-	std::streampos pos = in.tellg();
-
-	in.seekg(0, std::ios::beg);
-	int32_t header = 0;
-	for (int i = 0; i < 3 && !in.eof(); i++) {
-		char c;
-		in.get(c);
-		header <<= 8;
-		header |= (BYTE)c;
-	}
-
-	bool ret = header == 0xEFBBBF;
-	if (!ret)
-		in.seekg(pos);
-
-	return ret;
 }
-int SizeOfUnicode(BYTE header) {
-	char size = 0;
-	for (BYTE t = header; t & 0x80; t <<= 1, size++);
-	return size;
-}
-int32_t PeekFromUtf8(std::fstream& in) {
-	std::istream::int_type check = in.peek();
-	if (check == EOF)
-		return 0;
-	char c;
-	in.get(c);
-	int size = SizeOfUnicode(c);
-	if (!size)
-		return (BYTE)c;
-	if (size == 8)
-		return 0;
-	int32_t ret = ((BYTE)c) & (0xFF >> (7 - size));
-	for (int i = 0; i < size - 1; i++) {
-		in.get(c);
-		if (c == EOF)
-			throw EndOfFileException();
-		ret <<= 6;
-		ret |= ((BYTE)c) & 0x3F;
-	}
-	return ret;
-}
-
-int SizeOfUtf32(UCHAR_32 c, std::array<BYTE, 4>& u8Array) {
-	if (c > 0x10FFFF)
-		throw IllegalUtf8Exception();
-	if (c <= 0x7F) {
-	    u8Array[0] = (BYTE)c;
-		return 1;
-	}
-	int size;
-	if (c <= 0x7FF)
-		size = 2;
-	else if (c <= 0xFFFF)
-		size = 3;
-	else
-		size = 4;
-	for (int i = size - 1; i > 0; i--) {
-		BYTE tail = c & 0x3F;
-		u8Array[i] = (BYTE)(0x80 | tail);
-		c >>= 6;
-	}
-	BYTE mask = (0xFF >> (8 - size)) << (8 - size);
-	u8Array[0] = (BYTE)(mask | (BYTE)c);
-    return size;
-}
-void WriteToUtf8(std::fstream& out, UCHAR_32 c) {
-	std::array<BYTE, 4> buffer = {};
-	int size = SizeOfUtf32(c, buffer);
-	for (int i = 0; i < size; i++)
-		out << buffer[i];
-}
+using namespace cbm::util;
 
 int main() {
 	std::fstream in("../source.txt");
 	std::fstream out("../target.txt", std::ios::out);
 	out << (BYTE)0xEF << (BYTE)0xBB << (BYTE)0xBF;
 
-	if (CheckIfUtf8(in)) {
+    auto& converterU8 = CODE_CONVERTER::BaseUTF8Converter();
+
+	if (converterU8.CheckStreamHeader(in)) {
 		while (!in.eof()) {
-			int32_t c = PeekFromUtf8(in);
+            UNICHAR c = converterU8.PeekFromStream(in);
 			if (c == 0)
 				break;
 			std::cout << c << std::endl;
-			WriteToUtf8(out, c);
+            converterU8.WriteToStream(out, c);
 		}
 	}
 
